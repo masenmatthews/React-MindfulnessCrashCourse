173ba7ae5d560c1be820c5d8b6a78561fb2bd991
Masen Matthews <masenmatthews@gmail.com>
2018-09-26 09:52:27 -0700

    change folder arrangement, create folders for assets and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Header from './Header';
+import Header from './components/Header';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
   return (
     <div>
       <h1>Mindfulness Crash Course</h1>
-      <Link to="/">Home</Link> |
+      <Link to="/">Home</Link> 
     </div>
   );
 }
